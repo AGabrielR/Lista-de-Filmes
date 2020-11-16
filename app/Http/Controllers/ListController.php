c96a4e78dc2fba5c1a143791ca43ed3d1da8d40f
AGabrielR <gabrielreis130@gmail.com>
2020-11-16 17:31:35 -0400

    adicionando uma importação Auth

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 use App\Models\Profile;
 use App\Models\moviesList;
